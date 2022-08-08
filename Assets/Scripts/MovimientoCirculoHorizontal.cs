6e8cc2cb8ecdc6e8500306dc3d923a08935f58cb
dcruz23 <dayra_cruz08@hotmail.com>
2022-08-08 16:18:16 -0500

    08/08/22 SALTA Y GIRA
    BALON SALTA Y GIRA

--- Assets/Scripts/MovimientoCirculoHorizontal.cs
+++ Assets/Scripts/MovimientoCirculoHorizontal.cs
@@ -28,17 +28,17 @@ public class MovimientoCirculoHorizontal : MonoBehaviour
         rb2d.velocity = new Vector2(movimientoH * velocidad, rb2d.velocity.y);
 
         //4. Sentido horizontal (para girar el render del jugador)
-        if (movimientoH > 0)
+        if (movimientoH > 0) // (Input.GetKey(KeyCode.LeftArrow))
         {
-            spRd.flipX = false;
-           // gameObject.transform.Rotate = new Vector3(0, 0, -1);
-            //gameObject.transform.Rotate(0, 0, -1); //if the player is, then rotate counterclockwise in the x axis    
+            //spRd.flipX = false;
+           
+            gameObject.transform.Rotate(0, 0, -1); //if the player is, then rotate counterclockwise in the x axis    
         }
-        else if (movimientoH < 0)
+        else if (movimientoH < 0) // (Input.GetKey(KeyCode.RightArrow))
         {
-            spRd.flipX = true;
-            //gameObject.transform.Rotate = new Vector3(0, 0, 1);
-            //gameObject.transform.Rotate(0, 0, 1); //if he is, then rotate clockwise in the x axis
+           // spRd.flipX = true;
+           
+            gameObject.transform.Rotate(0, 0, 1); //if he is, then rotate clockwise in the x axis
         }
 
         }
